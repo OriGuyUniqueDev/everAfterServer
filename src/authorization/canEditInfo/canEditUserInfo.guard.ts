a2af0e70495e2f0c56ec1fcb21ec14c74ba11d83
Ori Guy <origuydev@gmail.com>
2023-05-31 17:38:42 +0300

    update guard

--- src/authorization/canEditInfo/canEditUserInfo.guard.ts
+++ src/authorization/canEditInfo/canEditUserInfo.guard.ts
@@ -6,46 +6,42 @@ export class CanEditUserInfoGuard implements CanActivate {
     constructor(private reflector:Reflector){}
     canActivate(context: ExecutionContext): boolean  {
         let user =  context.switchToHttp().getRequest().user
-        let paramEmail = context.switchToHttp().getRequest().params.email
-        console.log('CanEditUserInfoGuard guard',user.typeOfUser);
+        let req = context.switchToHttp().getRequest()
+        let paramEmail = req.params.email
+        console.log('CanEditUserInfoGuard guard',context.switchToHttp().getRequest());
         
         switch (user.typeOfUser) {
             case 'business':
+                //enable to business user to search all the events
+                // if (req.route.path === '/events' && req.route.methods.get === true) {
+                //     return true
+                // }
                 //edit his own details
                 if (user.email === paramEmail) {
-                        console.log('CanEditUserInfoGuard guard - continue');
+                    
 
                     return true
                 }
                 //can edit user info
-                console.log(user);
                 for (let i = 0; i < user.connectedUsers.length; i++) {
                     if(user.connectedUsers[i] === paramEmail){
-                        console.log('CanEditUserInfoGuard guard - continue');
+                    
 
                         return true
                     }
                 }
                 return false
-                // user.connectedUsers.forEach((email:string) => {
-                //     if(email === paramEmail){
-                //         console.log('CanEditUserInfoGuard guard - continue');
-
-                //         return true
-                //     }else{
-                //         return false
-                //     }
-                // });
+    
             case 'private':
                 if (user.email === paramEmail) {
-                        console.log('CanEditUserInfoGuard guard - continue');
+                    
 
                     return true
                 }else{
                     return false
                 }
             default:
-                console.log('CanEditUserInfoGuard guard - continue');
+            
                 
                 return true
         }
--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -12,25 +12,30 @@ import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-// import { Roles } from 'src/users/entities/roles.decorator';
-// import { Role } from 'src/users/entities/role.enum';
+import { Role } from 'src/authorization/roleAuth/role.enum';
+import { Roles } from 'src/authorization/roleAuth/roles.decorator';
+import { CanEditUserInfoGuard } from 'src/authorization/canEditInfo/canEditUserInfo.guard';
+import { RolesGuard } from 'src/authorization/roleAuth/roles.guard';
+
 
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
   @UseGuards(JwtAuthGuard)
+  @Roles(Role.Admin,Role.Business,Role.Private)
   @Post()
-  // @Roles(Role.ADMIN)
   create(@Body() createEventDto: CreateEventDto) {
     return this.eventsService.create(createEventDto);
   }
-
+  @Roles(Role.Admin)
+  @UseGuards(JwtAuthGuard,CanEditUserInfoGuard,RolesGuard)
   @Get()
   findAll() {
     return this.eventsService.findAll();
   }
-
+  @Roles(Role.Admin,Role.Business,Role.Private)
+  @UseGuards(JwtAuthGuard,CanEditUserInfoGuard,RolesGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.eventsService.findOne(id);
