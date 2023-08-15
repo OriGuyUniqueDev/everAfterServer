9160af769dd3b0096b2e960cc7798c08ef1c7c2a
OriGuyUniqueDev <origuydev@gmail.com>
2023-08-15 14:34:51 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import { DevDatabaseModule } from './databases/dev-database.module';
 import { ProdDatabaseModule } from './databases/prod-database.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  // app.enableCors();
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
