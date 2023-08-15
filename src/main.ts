d265a4439c976f411158770786f53374a40e03b0
OriGuyUniqueDev <origuydev@gmail.com>
2023-08-15 14:43:28 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,19 @@ import { ProdDatabaseModule } from './databases/prod-database.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+    allowedHeaders: '*',
+    origin: '*',
   });
 
   await app.listen(3000);
